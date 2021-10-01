79f989f4aed26370442ea017674686965454cd50
blackrm <83229490+blackrm@users.noreply.github.com>
2021-10-01 19:38:08 -0400

    Add files via upload

---
+++ Obj2Array.js
@@ -0,0 +1,8 @@
+let person1 = {firstName: "Kitty", lastName: "Wuff", age: "20"};
+let person2 = {firstName: "Bratty", lastName: "Addi", age: "25"};
+
+var myResult1 = Object.entries(person1);
+var myResult2 = Object.entries(person2);
+
+console.log(myResult1);
+console.log(myResult2);
